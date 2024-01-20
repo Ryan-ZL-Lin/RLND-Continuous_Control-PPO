ab2117c651e9f7faea4f689d4c0544d3d858b1da
Ryan <lzlallen1980@gmail.com>
2024-01-20 17:54:26 +0800

    Add files via upload

---
+++ Dict_Hyperparams.py
@@ -0,0 +1,14 @@
+# Default hyperparameters
+
+
+
+ROLLOUT_LENGTH = 500 
+DISCOUNT = 0.99                     # Discount factor
+GAE_LAMBDA = 0.95
+OPTIMIZATION_EPOCHS = 10
+MINI_BATCH_SIZE = 64                # minibatch size
+PPO_RATIO_CLIP = 0.1
+GRADIENT_CLIP = 0.75
+HIDDEN_LAYERS = 32                  # Number of units for the layer 1 in the actor and critic model
+
+
